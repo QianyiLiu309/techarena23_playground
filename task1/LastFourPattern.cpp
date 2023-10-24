6166958044a30d74a42d9433a42c0c7a5283037b
杨泽嘉 <yangzejia@yangzejiadeMacBook-Air.local>
2023-10-24 23:57:05 +0100

    Add Four IN

---
+++ task1/LastFourPattern.cpp
@@ -0,0 +1,110 @@
+// This file contains a template for the implementation of Robo prediction
+// algorithm
+// Using two ID for searching
+
+#include "PredictionAlgorithm.hpp"
+#include <map>
+#include <bitset>
+#include <queue>
+
+struct RoboPredictor::RoboMemory {
+
+  std::uint32_t currentPlanet = 0;
+  std::uint32_t lastPlanet = 0;
+  std::uint32_t lasttwoPlanet = 0;
+  std::uint64_t lastthreePlanet = 0;
+  std::map<std::pair<std::pair<std::uint64_t,std::uint32_t>, std::uint32_t>, std::int16_t> total_records;
+};
+
+bool RoboPredictor::predictTimeOfDayOnNextPlanet(
+    std::uint64_t nextPlanetID, bool spaceshipComputerPrediction) {
+  // Robo can consult data structures in its memory while predicting.
+  // Example: access Robo's memory with roboMemory_ptr-><your RoboMemory
+  // content>
+
+  // Robo can perform computations using any data in its memory during
+  // prediction. It is important not to exceed the computation cost threshold
+  // while making predictions and updating RoboMemory. The computation cost of
+  // prediction and updating RoboMemory is calculated by the playground
+  // automatically and printed together with accuracy at the end of the
+  // evaluation (see main.cpp for more details).
+
+  // Simple prediction policy: follow the spaceship computer's suggestions
+
+  std::map<std::pair<std::pair<std::uint64_t,std::uint32_t>, std::uint32_t>, std::int16_t>& total_records = roboMemory_ptr->total_records;
+  
+  std::pair<std::uint64_t,std::uint32_t> lastPlanetPattern = std::make_pair((roboMemory_ptr->lastthreePlanet << 42) | (roboMemory_ptr->lasttwoPlanet << 21) |
+                                    (roboMemory_ptr->lastPlanet), roboMemory_ptr->currentPlanet);
+
+  std::pair<std::pair<std::uint64_t,std::uint32_t>, std::uint32_t> pair = std::make_pair(lastPlanetPattern, nextPlanetID);
+
+  if (total_records.find(pair) == total_records.end()) {
+    return spaceshipComputerPrediction;
+  }
+  else if (total_records[pair] > 0){
+    return 1;
+  }
+  else if (total_records[pair] < 0) {
+    return 0;
+  }
+  else{
+    return spaceshipComputerPrediction;
+  }
+}
+
+void RoboPredictor::observeAndRecordTimeofdayOnNextPlanet(
+    std::uint64_t nextPlanetID, bool timeOfDayOnNextPlanet) {
+  // Robo can consult/update data structures in its memory
+  // Example: access Robo's memory with roboMemory_ptr-><your RoboMemory
+  // content>
+
+  // It is important not to exceed the computation cost threshold while making
+  // predictions and updating RoboMemory. The computation cost of prediction and
+  // updating RoboMemory is calculated by the playground automatically and
+  // printed together with accuracy at the end of the evaluation (see main.cpp
+  // for more details).
+
+
+  std::map<std::pair<std::pair<std::uint64_t,std::uint32_t>, std::uint32_t>, std::int16_t>& total_records = roboMemory_ptr->total_records;
+
+  std::pair<std::uint64_t,std::uint32_t> lastPlanetPattern = std::make_pair((roboMemory_ptr->lastthreePlanet << 42) | (roboMemory_ptr->lasttwoPlanet << 21) |
+                                    (roboMemory_ptr->lastPlanet), roboMemory_ptr->currentPlanet);
+
+  std::pair<std::pair<std::uint64_t,std::uint32_t>, std::uint32_t> pair = std::make_pair(lastPlanetPattern, nextPlanetID);
+  
+  roboMemory_ptr->lastthreePlanet = roboMemory_ptr->lasttwoPlanet;
+  roboMemory_ptr->lasttwoPlanet = roboMemory_ptr->lastPlanet;
+  roboMemory_ptr->lastPlanet = roboMemory_ptr->currentPlanet;
+
+  roboMemory_ptr->currentPlanet = nextPlanetID << 1 | timeOfDayOnNextPlanet;
+
+  if (total_records.find(pair) == total_records.end()) {
+    total_records[pair] = 0;
+  }
+
+  if(timeOfDayOnNextPlanet == 1){
+    total_records[pair] += 1;
+  }else{
+    total_records[pair] -= 1;
+  }
+  
+}
+
+
+//------------------------------------------------------------------------------
+// Please don't modify this file below
+//
+// Check if RoboMemory does not exceed 64KiB
+static_assert(
+    sizeof(RoboPredictor::RoboMemory) <= 65536,
+    "Robo's memory exceeds 65536 bytes (64KiB) in your implementation. "
+    "Prediction algorithms using so much "
+    "memory are ineligible. Please reduce the size of your RoboMemory struct.");
+
+// Declare constructor/destructor for RoboPredictor
+RoboPredictor::RoboPredictor() {
+  roboMemory_ptr = new RoboMemory;
+}
+RoboPredictor::~RoboPredictor() {
+  delete roboMemory_ptr;
+}
